3a006ffda59b0bcdf29fcbe283104cb7f83641c3
Guilhem Saurel <guilhem.saurel@laas.fr>
2019-04-19 20:00:33 +0200

    packaging

--- setup.py
+++ setup.py
@@ -1,19 +1,17 @@
-from setuptools import setup, find_packages
+#!/usr/bin/env python2
+
+from distutils.core import setup
 
 setup(
     name='hqp',
     description='Hierarchichal Quadratic Programming',
-    version='1.0',
-
-    packages=find_packages(exclude=['data','tests','examples']),
-    
+    version='1.0.1',
+    packages=['hqp', 'hqp.multi_contact'],
+    include_package_data=True,
+    url='https://github.com/gepetto/hqp',
     author='Galo MALDONADO',
     author_email='galo.maldonado@laas.fr',
-    
-    #install_requires = [
-    #    "pinocchio"
-    #],
-
+    license='LGPL',
     classifiers=[
         'Intended Audience :: Science/Research',
         'Programming Language :: Python',
